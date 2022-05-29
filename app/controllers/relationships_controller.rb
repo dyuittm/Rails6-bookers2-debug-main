50f29e185a43280f4b5fc8ee73880079a7d5aac7
EC2 Default User <ec2-user@ip-172-31-92-105.ec2.internal>
2022-05-29 09:10:36 +0000

    [Finish]4-task

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,4 +1,6 @@
 class RelationshipsController < ApplicationController
+  before_action :authenticate_user!
+
   def create
     current_user.follow(params[:user_id])
     redirect_to request.referer
@@ -7,6 +9,16 @@ class RelationshipsController < ApplicationController
   def destroy
     current_user.unfollow(params[:user_id])
     redirect_to request.referer
+  end
+
+  def followings
+    user = User.find(params[:user_id])
+    @users = user.followings
+  end
+
+  def followers
+    user = User.find(params[:user_id])
+    @users = user.followers
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,16 +25,6 @@ class UsersController < ApplicationController
     end
   end
 
-  def followings
-    @user = User.find(params[:user_id])
-    @followings = @user.followings
-  end
-
-  def followers
-    @user = User.find(params[:user_id])
-    @followers = @user.followers
-  end
-
   private
 
   def user_params
