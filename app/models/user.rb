12528bd77212803ff25117260aef8320c5625e71
EC2 Default User <ec2-user@ip-172-31-92-105.ec2.internal>
2022-05-30 02:26:06 +0000

    [Update]5-task

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,6 +24,20 @@ class User < ApplicationRecord
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
 
+  def self.looks(search, word)
+    if search == "perfect_match"
+      @user = User.where("name LIKE?", "#{word}")
+    elsif search == "forward_match"
+      @user = User.where("name LIKE?", "#{word}%")
+    elsif search == "backword_match"
+      @user = User.where("name LIKE?", "%#{word}")
+    elsif search == "partial_match"
+      @user = User.where("name LIKE?", "%#{word}%")
+    else
+      @user = User.all
+    end
+  end
+
   def follow(user)
     relationships.create(followed_id: user.id)
   end
