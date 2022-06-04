89bec6ecb84158cc7b08ea88443566d7d01c850c
EC2 Default User <ec2-user@ip-172-31-92-105.ec2.internal>
2022-06-04 02:18:47 +0000

    [Finish]7c-task

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :books, dependent: :destroy
+  has_many :group_users
+  has_many :groups, through: :group_users
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
     get 'followings' => 'relationships#followings', as:'followings'
     get 'followers' => 'relationships#followers', as:'followers'
   end
+  resources :groups, except: [:destroy]
   get "search" => "searches#search"
   get "home/about" => "homes#about"
 
