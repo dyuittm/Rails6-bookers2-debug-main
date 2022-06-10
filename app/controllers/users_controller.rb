a4b1140d157fa59444e0d0952c1b3f43411dc8e3
EC2 Default User <ec2-user@ip-172-31-92-105.ec2.internal>
2022-06-10 12:01:44 +0000

    [Update]7d-task

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @books = @user.books
     @book_new = Book.new
+    @book = Book.find(params[:id])
   end
 
   def index
